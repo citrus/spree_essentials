fa222546bee14fefb5b21afdb2267ebb4f22becb
Spencer Steffen <spencer@citrusme.com>
2011-06-06 15:29:56 -0700

    moved integration case class to lib/test_helper

--- lib/spree_essentials/test_helper.rb
+++ lib/spree_essentials/test_helper.rb
@@ -16,3 +16,29 @@ require "selenium/webdriver"
 
 Capybara.default_driver   = :selenium
 Capybara.default_selector = :css
+
+# Define a bare test case to use with Capybara
+class ActiveSupport::IntegrationCase < ActiveSupport::TestCase
+  
+  include Capybara
+  include Rails.application.routes.url_helpers
+  
+  self.use_transactional_fixtures = false
+  
+  def assert_seen(text, opts={})
+    if opts[:within]
+      within(opts[:within]) do
+        assert has_content?(text)
+      end
+    else
+      assert has_content?(text)
+    end
+  end
+  
+  def assert_flash(key, text)
+    within(".flash.#{key}") do
+      assert_seen(text)
+    end
+  end
+
+end
