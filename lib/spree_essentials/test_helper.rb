9d14fa0dc9e262ad274ee22fc0c3a187848c239f
Spencer Steffen <spencer@citrusme.com>
2011-06-08 11:23:19 -0700

    added translation test helper

--- lib/spree_essentials/test_helper.rb
+++ lib/spree_essentials/test_helper.rb
@@ -17,6 +17,7 @@ require "selenium/webdriver"
 Capybara.default_driver   = :selenium
 Capybara.default_selector = :css
 
+
 # Define a bare test case to use with Capybara
 class ActiveSupport::IntegrationCase < ActiveSupport::TestCase
   
@@ -24,7 +25,19 @@ class ActiveSupport::IntegrationCase < ActiveSupport::TestCase
   include Rails.application.routes.url_helpers
   
   self.use_transactional_fixtures = false
-   
+
+  # Checks for missing translations after each test
+  teardown do
+    unless source.blank?
+      assert !source.match('translation_missing'), "Translation Missing!"
+    end
+  end
+  
+  # An assertion for ensuring content has made it to the page.
+  #    
+  #    assert_seen "Site Title"
+  #    assert_seen "Peanut Butter Jelly Time", :within => ".post-title h1"
+  #      
   def assert_seen(text, opts={})
     if opts[:within]
       within(opts[:within]) do
@@ -35,19 +48,33 @@ class ActiveSupport::IntegrationCase < ActiveSupport::TestCase
     end
   end
   
+  # Asserts the proper flash message
+  #    
+  #    assert_flash :notice, "Post was successfully saved!"
+  #    assert_flash :error, "Oh No, bad things happened!"
+  #
   def assert_flash(key, text)
     within(".flash.#{key}") do
       assert_seen(text)
     end
   end
   
+  # Asserts the proper browser title
+  #    
+  #    assert_title "My Site - Is super cool"
+  #
   def assert_title(title)
     assert_seen title, :within => "head title"
   end
-
+  
+  # Asserts meta tags have proper content
+  #    
+  #    assert_meta :description, "So let me tell you about this one time..."
+  #    assert_meta :keywords, "seo, is, fun, jk."
+  #
   def assert_meta(tag, text)
     tag = find(:xpath, "//head/meta[@name='#{tag.to_s}']")
     assert_equal text, tag.native.attribute("content")
   end
-    
+      
 end
