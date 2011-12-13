76195cf08ab1d09ae90bfdd928ed7a41149d862e
Spencer Steffen <spencer@citrusme.com>
2011-12-13 10:07:37 -0800

    remove irrelevant js+css when running dummier

--- test/dummy_hooks/before_migrate.rb
+++ test/dummy_hooks/before_migrate.rb
@@ -4,3 +4,13 @@ run "rails g spree_essentials:install"
 
 copy_file "test.pdf", "public/test.pdf"
 copy_file "test.zip", "public/test.zip"
+
+# remove all stylesheets except core  
+%w(admin store).each do |ns|
+  js  = "app/assets/javascripts/#{ns}/all.js"
+  css = "app/assets/stylesheets/#{ns}/all.css"
+  remove_file js
+  remove_file css
+  template "#{ns}/all.js", js
+  template "#{ns}/all.css", css
+end
