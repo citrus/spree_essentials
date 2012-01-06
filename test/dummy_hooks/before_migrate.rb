e368c4a17fef21a30aa7b8efc68e98ce9d1391e9
Spencer Steffen <spencer@citrusme.com>
2012-01-06 13:28:25 -0800

    run rails generate after rake spree_core:install & migrating database [0.60.x]

--- test/dummy_hooks/before_migrate.rb
+++ test/dummy_hooks/before_migrate.rb
@@ -1,6 +1,5 @@
-# install spree and migrate db
-rake "spree_core:install"
-run  "rails g spree_essentials:install"
-
 copy_file "test.pdf", "public/test.pdf"
 copy_file "test.zip", "public/test.zip"
+
+# install spree and migrate db
+rake "spree_core:install"
