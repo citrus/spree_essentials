79d9d4e67b19dd2bf07844cdeae30565bee23c8d
Spencer Steffen <spencer@citrusme.com>
2012-03-06 10:59:46 -0800

    use built in rails rake task to install migration. we can remove the essentials_base shared generator too

--- lib/generators/spree_essentials/install_generator.rb
+++ lib/generators/spree_essentials/install_generator.rb
@@ -1,14 +1,12 @@
-require 'generators/essentials_base'
-
 module SpreeEssentials
   module Generators
-    class InstallGenerator < SpreeEssentials::Generators::EssentialsBase
+    class InstallGenerator < Rails::Generators::Base
       
       desc "Installs required migrations for spree_essentials"
       source_root File.expand_path("../../templates/db/migrate", __FILE__)
       
       def copy_migrations
-        migration_template "add_attachment_file_size_to_assets.rb", "db/migrate/add_attachment_file_size_to_assets.rb"
+        rake "spree_essentials:install:migrations"
       end
 
     end
