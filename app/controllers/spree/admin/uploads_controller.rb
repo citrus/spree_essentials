d1cb537a6ea57be6e4049bb1527ffcfcfb92a22e
Stephane Bounmy <stephanebounmy@gmail.com>
2012-09-30 11:38:15 +0200

    if ajax request, do not paginate

--- app/controllers/spree/admin/uploads_controller.rb
+++ app/controllers/spree/admin/uploads_controller.rb
@@ -1,16 +1,20 @@
 class Spree::Admin::UploadsController < Spree::Admin::ResourceController
-  
+
 	def index
 	  render :template => "spree/admin/uploads/#{request.xhr? ? 'picker' : 'index'}", :layout => !request.xhr?
   end
-  
+
   private
-  
+
     def collection
       params[:q] ||= {}
       params[:q][:sort] ||= "created_at.desc"
       @search = Spree::Upload.search(params[:q])
-      @collection = @search.result.page(params[:page]).per(Spree::Config[:orders_per_page])
+      if request.xhr?
+        @collection = @search.result
+      else
+        @collection = @search.result.page(params[:page]).per(Spree::Config[:orders_per_page])
+      end
     end
 
 end
