109d7ed0c3378ef4d8954b0fd04cb3bfc7deec6f
Ben-M <ben@maraney.com>
2012-10-21 16:56:42 +0200

    Update version, added spree_auth_devise for testing. Still need to fix failing integration tests.

--- test/dummy_hooks/before_migrate.rb
+++ test/dummy_hooks/before_migrate.rb
@@ -1,9 +1,13 @@
-# Require spree_essential_example in config/application.rb
+# Require spree_essential_example and spree_atuh in config/application.rb
 gsub_file "config/application.rb", 'require "spree_essentials"', %(require "spree_essentials"
-require "spree_essential_example")
+require "spree_essential_example"
+require "spree_auth_devise")
 
 # Install spree's migrations
 rake "spree:install:migrations"
+
+#Install spree auth migrations
+rake "spree_auth:install:migrations"
 
 # Mount the Spree::Core routes
 insert_into_file File.join('config', 'routes.rb'), :after => "Application.routes.draw do\n" do
