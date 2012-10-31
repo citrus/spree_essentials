711ea78197e91e498e0a19e93120ef5fbd2d1deb
Stephane Bounmy <stephanebounmy@gmail.com>
2012-10-31 09:13:57 +0100

    added legacy user initializer to dummy hook

--- test/dummy_hooks/before_migrate.rb
+++ test/dummy_hooks/before_migrate.rb
@@ -5,6 +5,9 @@ require "spree_essential_example")
 # Install spree's migrations
 rake "spree:install:migrations"
 
+# Copy spree user initializer
+template "initializers/spree_user.rb", "config/initializers/spree_user.rb"
+
 # Mount the Spree::Core routes
 insert_into_file File.join('config', 'routes.rb'), :after => "Application.routes.draw do\n" do
   "  # Mount Spree's routes\n  mount Spree::Core::Engine, :at => '/'\n"
