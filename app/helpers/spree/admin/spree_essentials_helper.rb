ddb6a04a2d31a4d58492cf13e4655ecc87399ffe
Stephane Bounmy <stephanebounmy@gmail.com>
2013-02-23 02:01:17 +0100

    added icon book

--- app/helpers/spree/admin/spree_essentials_helper.rb
+++ app/helpers/spree/admin/spree_essentials_helper.rb
@@ -1,18 +1,18 @@
 module Spree::Admin::SpreeEssentialsHelper
-  
+
   def inside_contents_tab?
     @inside_contents_tab ||= !request.fullpath.scan(Regexp.new(extension_routes.join("|"))).empty?
   end
-  
+
   def contents_tab
-    content_tag :li, :class => inside_contents_tab? ? 'selected' : nil do
-      link_to I18n.t('spree.admin.shared.contents_tab.content'), extension_routes.first
+    content_tag :li, :class => inside_contents_tab? ? 'selected tab-with-icon' : 'tab-with-icon' do
+      link_to_with_icon 'icon-book', I18n.t('spree.admin.shared.contents_tab.content'), extension_routes.first
     end
   end
-  
+
   def markdown_helper
     content_tag('em', :class => 'small markdown-helper') do
-      [ 
+      [
         t('essentials.parsed_with'),
         link_to("Markdown", "http://daringfireball.net/projects/markdown/basics", :onclick => 'window.open(this.href); return false')
       ].join(" ").html_safe
@@ -20,12 +20,12 @@ module Spree::Admin::SpreeEssentialsHelper
   end
 
 private
-  
+
   def extension_routes
     @extension_routes ||= SpreeEssentials.essentials.map { |key, cls|
       route = cls.tab[:route] || "admin_#{key}"
-      send("#{route}_path") rescue "##{key}"      
-    }.push(spree.admin_uploads_path)    
+      send("#{route}_path") rescue "##{key}"
+    }.push(spree.admin_uploads_path)
   end
-  
+
 end
